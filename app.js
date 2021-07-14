2462f24fabbcf472e7a117195dbb30e4de399174
moez barhoumi <moezmtk@gmail.com>
2021-07-14 19:47:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+/*function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}*/
+const router = require("./app/routes/router")
+app.use('/',router)
+
