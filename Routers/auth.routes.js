b834f60f9c0c2d5b9f4c365b477db7fd56ed4b43
moez barhoumi <moezmtk@gmail.com>
2021-07-15 11:20:58 +0100

    Add files via upload

---
+++ Routers/auth.routes.js
@@ -0,0 +1,22 @@
+const { verifySignUp } = require("../Middleware");
+const controller = require("../Controllers/auth.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/signup",[
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted
+    ],
+    controller.signup
+  );
+
+  app.post("/signin", controller.signin);
+};
