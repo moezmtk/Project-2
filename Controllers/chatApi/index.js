460beecbc4ef7513c278753e9cfff771c5d5bd52
moez barhoumi <moezmtk@gmail.com>
2021-08-06 13:04:29 +0100

    main

--- Controllers/chatApi/index.js
+++ Controllers/chatApi/index.js
@@ -2,7 +2,6 @@
 
 const server   = require("http");
 const io       = require("socket.io")(server);
-io.adapter(redisAdapter({ host : process.env.REDIS_HOST,port : 6379}))
 
 module.exports.io= io
 module.exports.server= server
